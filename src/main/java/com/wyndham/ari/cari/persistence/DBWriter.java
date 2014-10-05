487b6990133a94046934d97def372917cb2788ec
gbshiva <gbshiva@yahoo.com>
2014-10-05 00:53:21 -0400

    chagne toe timestamp

--- src/main/java/com/wyndham/ari/cari/persistence/DBWriter.java
+++ src/main/java/com/wyndham/ari/cari/persistence/DBWriter.java
@@ -24,23 +24,20 @@ import com.wyndham.ari.dao.Delivery;
 public class DBWriter implements CacheWriter {
 
 	private String url = null;
+	private String user = null;
+	private String pass = null;
 	static Logger logger = Logger.getLogger(DBWriter.class);
 	private volatile boolean write = true;
 	private Connection conn = null;
 
-	public DBWriter(String iurl, String user, String pass) {
+	public DBWriter(String iurl, String iuser, String ipass) {
 		try {
-			logger.info(" Connecting to DB  "+url +" "+user +" "+pass);
 
 			// Class.forName("com.mysql.jdbc.Driver");
 			url = iurl;
-			conn = DriverManager.getConnection(url, user, pass);
-			conn.setAutoCommit(true);
-			if (conn != null){
-				logger.info(" Db Connection established successfully to "+url +" "+user +" "+pass);
-			}else {
-				logger.error (" Unable to create DB connection");
-			}
+			user = iuser;
+			pass = ipass;
+
 		} catch (Exception ex) {
 			logger.error(ex);
 		}
@@ -53,7 +50,15 @@ public class DBWriter implements CacheWriter {
 
 	public void init() {
 		try {
-			// ds = setupDataSource(url);
+			logger.info(" Connecting to DB  " + url + " " + user + " " + pass);
+			conn = DriverManager.getConnection(url, user, pass);
+			conn.setAutoCommit(true);
+			if (conn != null) {
+				logger.info(" Db Connection established successfully to " + url
+						+ " " + user + " " + pass);
+			} else {
+				logger.error(" Unable to create DB connection");
+			}
 
 		} catch (Exception e) {
 			throw new CacheException("Couldn't conenct to DB " + url, e);
@@ -72,25 +77,21 @@ public class DBWriter implements CacheWriter {
 	public synchronized void write(final Element element) throws CacheException {
 
 		try {
-			
+
 			Delivery dlvry = (Delivery) element.getObjectValue();
 			PreparedStatement insert = null;
-			 String insertstmt = " insert into ARI_DELIVERY_HEADER(REQUEST_ID, BRAND_ID,PROPERTY_ID,MSG_STATUS_ID, MSG_TIMESTAMP,MSG_SUBJECT_ID) values (?,?,?,?,?,?)";
-			 insert = conn.prepareStatement(insertstmt);
-			 insert.setInt(1, (int) dlvry.getReqId());
-			 insert.setString(2, dlvry.getBrandId());
-			 insert.setString(3, dlvry.getPropertyId());
-			 insert.setInt(4, dlvry.getMessageStatusId());
-			 insert.setTimestamp(5, new Timestamp(dlvry.getsourceTimeStamp_long()));
-			 insert.setInt(6, 1);
-			 insert.execute();
-			 insert.close();
-			
-			
-			
-			
-			
-			
+			String insertstmt = " insert into ARI_DELIVERY_HEADER(REQUEST_ID, BRAND_ID,PROPERTY_ID,MSG_STATUS_ID, MSG_TIMESTAMP,MSG_SUBJECT_ID) values (?,?,?,?,?,?)";
+			insert = conn.prepareStatement(insertstmt);
+			insert.setInt(1, (int) dlvry.getReqId());
+			insert.setString(2, dlvry.getBrandId());
+			insert.setString(3, dlvry.getPropertyId());
+			insert.setInt(4, dlvry.getMessageStatusId());
+			insert.setTimestamp(5,
+					new Timestamp(dlvry.getsourceTimeStamp_long()));
+			insert.setInt(6, 1);
+			insert.execute();
+			insert.close();
+
 		} catch (Exception ex) {
 			logger.error(ex);
 		}
