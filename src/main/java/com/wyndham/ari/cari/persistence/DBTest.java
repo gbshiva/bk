bfc1a1b523977b51a856880986f33cf220d44e51
gbshiva <gbshiva@yahoo.com>
2014-10-05 00:29:12 -0400

    chagne toe timestamp

--- src/main/java/com/wyndham/ari/cari/persistence/DBTest.java
+++ src/main/java/com/wyndham/ari/cari/persistence/DBTest.java
@@ -14,9 +14,20 @@ public class DBTest {
 			connection = DriverManager.getConnection(
 					args[0],
 					args[1], args[2]);
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("insert into ARI_DELIVERY_HEADER values ( 1, '1','1','1',1,''27/02/2002 15:51.12.539880',1,'',1,1,'N','1','1',1,'','','')");
-			stmt.close();
+			
+			connection.setAutoCommit(true);
+			PreparedStatement insert = null;
+			 String insertstmt = " insert into ARI_DELIVERY_HEADER(REQUEST_ID, BRAND_ID,PROPERTY_ID,MSG_STATUS_ID, MSG_TIMESTAMP,MSG_SUBJECT_ID) values (?,?,?,?,?,?)";
+			 insert = connection.prepareStatement(insertstmt);
+			 insert.setInt(1, 1);
+			 insert.setString(2, "1");
+			 insert.setString(3, "1");
+			 insert.setInt(4, 1);
+			 insert.setTimestamp(5, new Timestamp(1412480813));
+			 insert.setInt(6, 1);
+			 insert.execute();
+			 insert.close();
+			 
 			} else 
 				
 			{
