1f530db9413325d4ae27dd307903990a11328961
gbshiva <gbshiva@yahoo.com>
2014-10-04 22:00:20 -0400

    DB Sample

--- src/main/java/com/wyndham/ari/cari/persistence/DBTest.java
+++ src/main/java/com/wyndham/ari/cari/persistence/DBTest.java
@@ -7,14 +7,22 @@ public class DBTest {
 	public static void main(String[] args) {
 
 		try {
-			Class.forName("org.postgresql.Driver");
+			//Class.forName("org.postgresql.Driver");
 			Connection connection = null;
+			if  (args.length >= 3 ){ 
+				System.out.print(args[0]+args[1]+args[2]);
 			connection = DriverManager.getConnection(
 					args[0],
 					args[1], args[2]);
 			Statement stmt = connection.createStatement();
 			stmt.executeUpdate("insert into ARI_DELIVER_HEADER values ( 1, '1','1','1',1,'',1,'',1,1,'N','1','1',1,'','','')");
 			stmt.close();
+			} else 
+				
+			{
+				System.out.println("Error");
+				
+			}
 
 		} catch (Exception e) {
 
