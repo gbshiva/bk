1a5198c36697857cbf5b7a7584bc5ab7fadc08c9
gbshiva <gbshiva@yahoo.com>
2014-10-06 13:18:01 -0400

    add mroe log stmts

--- src/main/java/com/wyndham/ari/helper/Instrumentation.java
+++ src/main/java/com/wyndham/ari/helper/Instrumentation.java
@@ -22,7 +22,7 @@ public class Instrumentation {
                 .formatFor(Locale.US)
                 .convertRatesTo(TimeUnit.SECONDS)
                 .convertDurationsTo(TimeUnit.MILLISECONDS)
-                .build(new File("/tmp/metrics"));
+                .build(new File("/terracotta/metrics"));
 	
 		
 	
